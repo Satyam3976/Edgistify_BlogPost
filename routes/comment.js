d5600d4ccdaba99f26d431bc56f938281adfd022
Satyam Chaudhary <chaudhary.satyam3976@gmail.com>
2020-03-01 23:41:30 +0530

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const commentController = require('../controllers/comment');
 
 router.post('/addComment', commentController.addComment);
-// router.get('/allComments', commentController.getCommentById);
+
 
 module.exports = router;
