4079ef24115419b09b5881d04e355442ee2a897c
Satyam Chaudhary <chaudhary.satyam3976@gmail.com>
2020-03-01 23:42:16 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -5,9 +5,6 @@ const postController = require('../controllers/post');
 router.post('/createPost', postController.createPost);
 router.get('/displayPosts', postController.displayPosts);
 router.post('/displayPost', postController.displayPost);
-// router.post('/acceptGig/:id', gigsController.acceptGig);
-router.delete('/deletePost',postController.deletePost);
-// router.get('/userGigs',gigsController.displayUserGigs);
 
 
 module.exports = router;
